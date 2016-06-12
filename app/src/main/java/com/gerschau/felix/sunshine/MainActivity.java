792ed1535d1f981512aaae0b9767f60a63c55a05
Felix <felix.gerschau@gmail.com>
2016-06-12 20:18:32 +0200

    Add vcs support

--- app/src/main/java/com/gerschau/felix/sunshine/MainActivity.java
+++ app/src/main/java/com/gerschau/felix/sunshine/MainActivity.java
@@ -1,11 +1,9 @@
 package com.gerschau.felix.sunshine;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
